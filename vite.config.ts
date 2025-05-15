b79d226c9ba729144b8a5dd4b40a31cbab408521
sandeep Chetnani <sandeep.lekhraj@medibuddy.in>
2025-05-15 19:46:12 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bill-buddy-bite/" : "/",
+  base: mode === "production" ? "/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+//https://sandeepchetnani.github.io/bill-buddy-bite/
